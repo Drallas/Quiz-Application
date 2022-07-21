b9349598ee02d0a9ba483f4d95b9bcdf22f0d980
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-21 18:42:37 +0000

    sort

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+import asyncio
 import unittest
 
 import triviaapi
-import asyncio
+
 
 class TestInput(unittest.TestCase):
+    '''Dummy test'''
     def test_trivia(self):
         # Test get_random_questions to return a list of questions from 1, 1, easy
         # Asseert  that get_random_questions return a dictionary with the questions
