f85beefbb36edf0401bc02058c6e0ec7b25fd71e
Allard <allard.schmidt@inergy.nl>
2022-07-21 14:24:25 +0000

    imporve naming..

--- triviaapi.py
+++ triviaapi.py
@@ -1,7 +1,7 @@
 from trivia import trivia
 
 
-categories: dict = {
+question_category: dict = {
     0: "All Categories",
     1: "General Knowledge",
     2: "Entertainment: Books",
@@ -9,7 +9,7 @@ categories: dict = {
     4: "Entertainment: Music"
 }
 
-difficulties: dict = {
+question_difficulty: dict = {
     0: "Any Difficulty",
     1: "Easy",
     2: "Medium",
@@ -17,10 +17,12 @@ difficulties: dict = {
 }
 
 # Get Random Quiz questions via the Trivia API
+
+
 async def get_random_questions(
-    questions_count: int,
-    category_key: int,
-    difficulty_value: str) -> dict:
+        questions_count: int,
+        category_key: int,
+        difficulty_value: str) -> dict:
     """
     Get a list of random questions from the Trivia API
     """
@@ -36,5 +38,18 @@ async def get_random_questions(
         difficulty=difficulty_value.lower(),
         quizType='multiple'
     )
-    
+
     print(questions)
+
+    # Tmp: response format
+    # [{
+    # 'category': 'Entertainment: Music',
+    # 'type': 'multiple',
+    # 'difficulty': 'hard',
+    # 'question': 'Which band is the longest active band in the world with no breaks or line-up changes?',
+    # 'correct_answer': 'U2',
+    # 'incorrect_answers': ['Radiohead', 'Rush', 'Rolling Stones']
+    # },
+
+    # {'category': 'Entertainment: Music', 'type': 'multiple', 'difficulty': 'hard', 'question': 'Which of the following is NOT a real song from the band Thousand Foot Krutch?', 'correct_answer': 'Limitless Fury', 'incorrect_answers': ['Let The Sparks Fly', 'Down', 'Give Up The Ghost']},
+    # ]
