ebb64fe39971fee84a779b2702620436c92ef953
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-22 07:29:20 +0000

    played..

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
+'''Tests'''
 import asyncio
 import unittest
 
 import triviaapi
 
 
+# * Alert: Add real tests
 class TestInput(unittest.TestCase):
     '''Dummy test'''
     def test_trivia(self):
-        # Test get_random_questions to return a list of questions from 1, 1, easy
-        # Asseert  that get_random_questions return a dictionary with the questions
-        # Assert that the dictionary has the correct number of questions.
-        type(asyncio.run(triviaapi.get_random_questions(1, 1, "easy"))) == dict
+        '''Dummy test'''
+        self.assertEqual(1, 1)
